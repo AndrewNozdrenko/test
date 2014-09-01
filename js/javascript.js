bc9db672a9facb96b9ffe875fa41f8626fd33e4c
Andrey Nozdrenko <anno@qapint.com>
2014-09-01 09:44:27 +0300

    formated code

--- js/javascript.js
+++ js/javascript.js
@@ -12,21 +12,28 @@ var slider = document.getElementById('slider'),
 const space = 700;
 
 function activatedAnimate(getPosition){
-		if(bodyElement.classList.contains('animate')){
-			bodyElement.classList.remove('animate');
-		}
-		else{
-			bodyElement.classList.add('animate');
-		}
+	if(bodyElement.classList.contains('animate')){
+		bodyElement.classList.remove('animate');
+	}
+	else{
+		bodyElement.classList.add('animate');
+	}
 	clickOnSlider = false;
 	function checkFooterDescription(positionElement){
 		var footerText = document.getElementsByClassName("footer-text")[0];
-		switch (positionElement){
-			case 0 : footerText.innerHTML = "aXarelto is contraindicated in lesions or conditions, if they are considered to be a significant <br>risk for major<br> bleeding, which may include the presence of malignant neoplasms at high risk of bleeding. bOutcome: symptomatic, recurrent VTE."; break;
-			case -space : footerText.innerHTML = "<sup>a</sup>Patients participating in EINSTEIN DVT trial had confirmed acute symptomatic DVT without symptomatic PE and completed two measures of treatment satisfaction;<br> ACTS (Anti Clot Treatment Scale) and TSQM (Treatment Satisfaction Questionnaire for Medication) in follow up visits.<sup>5,16</sup><br> <sup>b</sup>Score values given for burdens and benefits measured by ACTS; effectiveness, side effects, convenience and global satisfaction measured by TSQM scale.<sup>16</sup>"; break;
-			case -space*2 : footerText.innerHTML = "Numbers provided are based on general epidemiological reserch and on trial experience from the Phase III Programme whith Xarelto. Helthcare practice<br> may be different in your country(both before and after introduction of Xarelto), and thus the estimation may not reflect your country's perspective<br> Actual costs are not included"; break;
+		switch(positionElement){
+			case 0 :
+				footerText.innerHTML = "aXarelto is contraindicated in lesions or conditions, if they are considered to be a significant <br>risk for major<br> bleeding, which may include the presence of malignant neoplasms at high risk of bleeding. bOutcome: symptomatic, recurrent VTE.";
+				break;
+			case -space :
+				footerText.innerHTML = "<sup>a</sup>Patients participating in EINSTEIN DVT trial had confirmed acute symptomatic DVT without symptomatic PE and completed two measures of treatment satisfaction;<br> ACTS (Anti Clot Treatment Scale) and TSQM (Treatment Satisfaction Questionnaire for Medication) in follow up visits.<sup>5,16</sup><br> <sup>b</sup>Score values given for burdens and benefits measured by ACTS; effectiveness, side effects, convenience and global satisfaction measured by TSQM scale.<sup>16</sup>";
+				break;
+			case -space * 2 :
+				footerText.innerHTML = "Numbers provided are based on general epidemiological reserch and on trial experience from the Phase III Programme whith Xarelto. Helthcare practice<br> may be different in your country(both before and after introduction of Xarelto), and thus the estimation may not reflect your country's perspective<br> Actual costs are not included";
+				break;
 		}
 	}
+
 	checkFooterDescription(getPosition);
 
 	balance[0].classList.remove('left-balanse-right-translate');
@@ -54,20 +61,19 @@ slider.onmouseup = function(){
 slider.onmousemove = function(e){
 	var temp = coordinateY - e.pageY,
 		stringOfPosition = display.style.webkitTransform,
-		getPosition = stringOfPosition.substring(stringOfPosition.indexOf('(')+1, stringOfPosition.indexOf('px'));
+		getPosition = stringOfPosition.substring(stringOfPosition.indexOf('(') + 1, stringOfPosition.indexOf('px'));
 
-	if(getPosition === '')
-	{
+	if(getPosition === ''){
 		getPosition = 0;
 	}
-	if(temp >= 50 && clickOnSlider == true && getPosition > -space*2){
+	if(temp >= 50 && clickOnSlider == true && getPosition > -space * 2){
 		getPosition = parseInt(getPosition) - space;
-		display.style.webkitTransform = 'translateY('+ getPosition + 'px)';
+		display.style.webkitTransform = 'translateY(' + getPosition + 'px)';
 		activatedAnimate(getPosition);
 	}
 	else if(temp <= -50 && clickOnSlider == true && getPosition < 0){
 		getPosition = parseInt(getPosition) + space;
-		display.style.webkitTransform = 'translateY('+ getPosition + 'px)';
+		display.style.webkitTransform = 'translateY(' + getPosition + 'px)';
 		activatedAnimate(getPosition);
 	}
 }
@@ -118,20 +124,20 @@ inputs[0].oninput = function(){
 	var a, b, c, w, d, g, enterValue, sum, multiply;
 	enterValue = this.value;
 
-	b = 182*(0.122*enterValue);
-	c = 16*(enterValue-(0.122*enterValue));
+	b = 182 * (0.122 * enterValue);
+	c = 16 * (enterValue - (0.122 * enterValue));
 	sum = b + c;
 	outputs[0].value = sum.toFixed(2);
 
-	w = enterValue-(enterValue*0.122);
+	w = enterValue - (enterValue * 0.122);
 	d = 14;
-	multiply = d*w;
+	multiply = d * w;
 	outputs[1].value = multiply.toFixed(2);
 
 	g = c * 0.064;
 	outputs[2].value = g.toFixed(2);
 
-	a = 3*(0.52*enterValue);
+	a = 3 * (0.52 * enterValue);
 	outputs[3].value = a.toFixed(2);
 }
 
@@ -139,12 +145,12 @@ inputs[1].oninput = function(){
 	var b, c, w, d, sum, multiply, enterValue, result;
 	enterValue = this.value;
 
-	b = 182*(enterValue*0.176);
-	c = 16*(enterValue-0.176*enterValue);
+	b = 182 * (enterValue * 0.176);
+	c = 16 * (enterValue - 0.176 * enterValue);
 	sum = b + c;
 	outputs[4].value = sum.toFixed(2);
 
-	w = enterValue-(enterValue*0.176);
+	w = enterValue - (enterValue * 0.176);
 	d = 14;
 	multiply = w * d;
 	outputs[5].value = multiply.toFixed(2);
@@ -157,18 +163,18 @@ inputs[1].oninput = function(){
 buttons[1].onclick = function(e){
 	bodyElement.classList.remove('animate');
 	e.preventDefault();
-	display.style.webkitTransform = 'translateY('+ 0 + 'px)';
+	display.style.webkitTransform = 'translateY(' + 0 + 'px)';
 	activatedAnimate(0);
 }
 buttons[2].onclick = function(e){
 	e.preventDefault();
-	display.style.webkitTransform = 'translateY('+ -space + 'px)';
+	display.style.webkitTransform = 'translateY(' + -space + 'px)';
 	activatedAnimate(-space);
 	bodyElement.classList.remove('animate');
 }
 buttons[3].onclick = function(e){
 	e.preventDefault();
-	display.style.webkitTransform = 'translateY('+ -space*2 + 'px)';
-	activatedAnimate(-space*2);
+	display.style.webkitTransform = 'translateY(' + -space * 2 + 'px)';
+	activatedAnimate(-space * 2);
 	bodyElement.classList.remove('animate');
 }
